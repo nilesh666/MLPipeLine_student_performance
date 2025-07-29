4bb298b42754858bd502a2916c29420aefc8b5b1
nilesh666 <nileshnandan.ts@gmail.com>
2025-07-29 19:38:08 +0530

    Model Trainer Finished

---
+++ src/components/model_trainer.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_path = os.path.join('./artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initiate_model_trainer(self, train_array, test_array):
+        try: 
+            logging.info("Input the data and split")
+            x_train, y_train, x_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+
+            model_report, best_model= evaluate_models(x_train = x_train, 
+                                                y_train = y_train,
+                                                x_test = x_test,
+                                                y_test = y_test,
+                                                models = models,
+                                                )
+            
+            best_model = model_report[model_report['R2_test'] == model_report['R2_test'].max()]
+            best_model_name = best_model['Model'].iloc[0]
+            best_model_score = best_model['R2_test'].iloc[0]
+
+            if best_model_score < 0.6:
+                raise CustomException("Model Poor")
+            
+            save_object(
+                file_path = self.model_trainer_config.trained_model_path,
+                ob = best_model
+            )
+            
+            logging.info(f"Models trained and the best model is {best_model_name} and the score is {best_model_score}!!!")
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 import dill
 
 def save_object(file_path, ob):
@@ -12,3 +14,48 @@ def save_object(file_path, ob):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluation_metrics(true, predicted):
+  mae = mean_absolute_error(true, predicted)
+  mse = mean_squared_error(true, predicted)
+  rmse = np.sqrt(mse)
+  r2_square = r2_score(true, predicted)
+  return mae, rmse, r2_square
+
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    model_list = []
+    results = []
+    best_model = None
+    best_r2 = float('-inf')  # to track best R² score
+
+    for i in range(len(models)):
+        model_name = list(models.keys())[i]
+        model = list(models.values())[i]
+
+        model.fit(x_train, y_train)
+
+        y_train_pred = model.predict(x_train)
+        y_test_pred = model.predict(x_test)
+
+        model_train_mae, model_train_rmse, model_train_r2 = evaluation_metrics(y_train, y_train_pred)
+        model_test_mae, model_test_rmse, model_test_r2 = evaluation_metrics(y_test, y_test_pred)
+
+        results.append({
+            "Model": model_name,
+            "MAE_train": model_train_mae,
+            "RMSE_train": model_train_rmse,
+            "R2_train": model_train_r2,
+            "MAE_test": model_test_mae,
+            "RMSE_test": model_test_rmse,
+            "R2_test": model_test_r2
+        })
+
+        # Track best model based on test R²
+        if model_test_r2 > best_r2:
+            best_r2 = model_test_r2
+            best_model = model
+
+    result_data = pd.DataFrame(results)
+    result_data.to_csv("./artifacts/model_results.csv", index=False)
+
+    return result_data, best_model
